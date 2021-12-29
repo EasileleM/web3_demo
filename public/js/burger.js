6bd75f96465a2211ef51a6f18de99266bec5400c
Savva Dzhumaliev <savva.dzhumaliev@sbermarket.ru>
2021-12-29 23:54:55 +0400

    roadmap modal

--- public/js/burger.js
+++
@@ -1,8 +0,0 @@
-function toggleBurgerMenu() {
-    const mobileLinks = document.querySelector('.header__mobile-links')
-    mobileLinks.classList.toggle('header__mobile-links-active')
-}
-
-document.querySelectorAll('.header__mobile-link').forEach(mobileLink => {
-    mobileLink.addEventListener('click', toggleBurgerMenu)
-})
